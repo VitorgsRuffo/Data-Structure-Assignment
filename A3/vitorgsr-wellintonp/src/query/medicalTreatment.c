c5069e3e50f0a7ecec6d2cc921072a7793f74808
VitorgsRuffo <vitorruffo19cc@gmail.com>
2020-10-14 14:54:52 -0300

    continuing soc query command

--- A3/vitorgsr-wellintonp/src/query/medicalTreatment.c
+++ A3/vitorgsr-wellintonp/src/query/medicalTreatment.c
@@ -11,8 +11,14 @@ typedef struct {
 }Address;
 
 typedef struct {
+    double x;
+    double y;
+}CenterOfMass;
+
+typedef struct {
     Address address;
     double x, y, w, h;
+    CenterOfMass centerOfMass;
 }House;
 
 typedef struct {
@@ -20,15 +26,20 @@ typedef struct {
 }CovidBlock;
 
 typedef struct {
-    Info healthCenter;
+    HealthCenter healthCenter;
     double distance;
 }NearHealthCenter;
 
 void readBlockAttributes(Info blockInfo, CovidBlock *block);
 void calculateHouseLocation(CovidBlock *block, House *house);
 char* buildBlueHouseTag(House* house);
+void copyHealthCenterListNodesInfoToArray(List healthCenterList, NearHealthCenter* nearHealthCenters);
+void calculateDistanceFromHouseToHealthCenters(House* house, NearHealthCenter* nearHealthCenters, int healthCentersAmount);
 char* buildLineSegmentTag(double x, double y, House* house);
 void writeHealthCenterCoordinatesOnTxt(File txt, int i, double x, double y, double distance);
+
+
+////////////////////////////////////////////////////////////Refatorar.
 
 
 void executeMedicalTreatmentSearching(char* command, Drawing Dr, File txt){
@@ -37,7 +48,7 @@ void executeMedicalTreatmentSearching(char* command, Drawing Dr, File txt){
 
     House house;
     int K;
-    sscanf(&command[3], "%d %s %c %d", &K, house.address.cep, &house.address.face, &house.address.number);
+    sscanf(&command[4], "%d %s %c %d", &K, house.address.cep, &house.address.face, &house.address.number);
     house.w = 15.0; house.h = 15.0;
 
 
@@ -56,30 +67,26 @@ void executeMedicalTreatmentSearching(char* command, Drawing Dr, File txt){
     char* blueHouseTag = buildBlueHouseTag(&house);
     List queryElementsList = getQueryElementsList(Dr);
     insert(queryElementsList, blueHouseTag);
-    
-    
-    //Sort:
-        //get lista postos
-        //tamanho lista postos
-        //NearHealthCenter healthCentersDistance[listLength];
-        //copiar infos dos nos da lista para o campo healthCenter dos elementos do vetor.
-        //for no vetor pra calcular a distancia entre o meio da casa e o posto atual (guardar no campo distance do elemento atual do vetor)
 
-        //sort no vetor considerando o campo distance do elemento do vetor.
+        
+    List healthCenterList = getHealthCenterList(Dr);
+    int healthCentersAmount = length(healthCenterList);
+    NearHealthCenter nearHealthCenters[healthCentersAmount];
+    copyHealthCenterListNodesInfoToArray(healthCenterList, nearHealthCenters);
+    calculateDistanceFromHouseToHealthCenters(&house, nearHealthCenters, healthCentersAmount);
+    //ordenar o vetor de acordo com o campo distance do elemento do vetor.
 
     int healthCenterX, healthCenterY;
     char* lineSegmentTag;
     
     for(int i = 0; i < K; i++){
 
-        healthCenterX = atof(getHealthCenterX(healthCenters[i]));
-        healthCenterY = atof(getHealthCenterY(healthCenters[i]));
+        healthCenterX = atof(getHealthCenterX(nearHealthCenters[i].healthCenter));
+        healthCenterY = atof(getHealthCenterY(nearHealthCenters[i].healthCenter));
         lineSegmentTag = buildLineSegmentTag(healthCenterX, healthCenterY, &house);
         insert(queryElementsList, lineSegmentTag);
-        writeHealthCenterCoordinatesOnTxt(txt, i, healthCenterX, healthCenterY, healthCenters[i].distance);
+        writeHealthCenterCoordinatesOnTxt(txt, i, healthCenterX, healthCenterY, nearHealthCenters[i].distance);
     }
-
-    //Refatorar.
 
 }
 
@@ -126,6 +133,9 @@ void calculateHouseLocation(CovidBlock *block, House *house){
             printf("A face da quadra (%s) e invalida.\n", house->address.cep);
     }
 
+    house->centerOfMass.x = house->x + (house->w / 2.0);
+    house->centerOfMass.y = house->y + (house->h / 2.0);
+
     return;
 }
 
@@ -156,25 +166,51 @@ void calculateHouseLocationOnWestFace(CovidBlock *block, House *house){
 char* buildBlueHouseTag(House* house){
 
     char* houseTag = (char*) malloc(200 * sizeof(char));
-    if(isElementNull(houseTag, "houseTag", "buildHouseTag"))
+    if(isElementNull(houseTag, "houseTag", "buildBlueHouseTag"))
         return NULL;
 
     sprintf(houseTag, "\t<rect width=\"%lf\" height=\"%lf\" x=\"%lf\" y=\"%lf\" stroke=\"white\" stroke-width=\"1\" fill=\"lightblue\" />\n", house->w, house->h, house->x, house->y);
-
     return houseTag;
+}
+
+void copyHealthCenterListNodesInfoToArray(List healthCenterList, NearHealthCenter* nearHealthCenters){
+    Node NODE = getFirst(healthCenterList);
+    if(isElementNull(NODE, "NODE", "copyHealthCenterListNodeInfoToArray | getFirst"))
+        return;
+    
+    Info healthCenterInfo = NULL;
+
+    int i = 0;
+    while(NODE != NULL){
+
+        healthCenterInfo = get(healthCenterList, NODE);
+        nearHealthCenters[i].healthCenter = healthCenterInfo;
+
+        NODE = getNext(healthCenterList, NODE);
+        i++;   
+    }
+      
+}
+
+void calculateDistanceFromHouseToHealthCenters(House* house, NearHealthCenter* nearHealthCenters, int healthCentersAmount){
+    double healthCenterX, healthCenterY;  
+
+    for(int i = 0; i<healthCentersAmount; i++){
+        healthCenterX = atof(getHealthCenterX(nearHealthCenters[i].healthCenter));
+        healthCenterY = atof(getHealthCenterY(nearHealthCenters[i].healthCenter));
+        
+        nearHealthCenters[i].distance = sqrt(pow((healthCenterX - house->x), 2) + pow((healthCenterY - house->y), 2));
+    }
 }
 
 char* buildLineSegmentTag(double x, double y, House* house){
 
-    char* LineSegmentTag = (char*) malloc(200 * sizeof(char));
-    if(isElementNull(LineSegmentTag, "LineSegmentTag", "buildLineSegmentTag"))
+    char* lineSegmentTag = (char*) malloc(200 * sizeof(char));
+    if(isElementNull(lineSegmentTag, "lineSegmentTag", "buildLineSegmentTag"))
         return NULL;
 
-    double houseCenterX = house->x + (house->w/2.0);
-    double houseCenterY = house->y + (house->h/2.0);
-
-    sprintf(LineSegmentTag, "<line x1=\"%lf\" y1=\"%lf\" x2=\"%lf\" y2=\"%lf\" stroke=\"black\" stroke-width=\"0.8\" stroke-dasharray=\"5,5\"/>", houseCenterX, houseCenterY, x, y);
-    return LineSegmentTag;
+    sprintf(lineSegmentTag, "<line x1=\"%lf\" y1=\"%lf\" x2=\"%lf\" y2=\"%lf\" stroke=\"black\" stroke-width=\"0.8\" stroke-dasharray=\"5,5\"/>", house->centerOfMass.x, house->centerOfMass.y, x, y);
+    return lineSegmentTag;
 }
 
 void writeHealthCenterCoordinatesOnTxt(File txt, int i, double x, double y, double distance){
